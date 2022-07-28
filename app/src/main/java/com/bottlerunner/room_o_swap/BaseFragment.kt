50c11a2e9b90e2a10d4d5c9682eeebbc5fd730d4
shashankRanaNoidaWala <101785829+shashankRanaNoidaWala@users.noreply.github.com>
2022-07-28 11:19:30 +0530

    setting up sign up

---
+++ app/src/main/java/com/bottlerunner/room_o_swap/BaseFragment.kt
@@ -0,0 +1,65 @@
+package com.bottlerunner.room_o_swap
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+
+open class BaseFragment(var layoutId: Int) : Fragment() {
+
+    lateinit var currContext:Context
+    private lateinit var mProgressDialog:Dialog
+    private var doubleBackToExitPressedOnce:Boolean =false
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        currContext=context
+    }
+
+    fun showProgressDialog(text: String){                                                         //WHAT IS THISSS??
+        mProgressDialog = Dialog(currContext)
+        mProgressDialog.setContentView(R.layout.dialog_progress)
+        mProgressDialog.findViewById<TextView>(R.id.tv_progress_text).text = text
+        mProgressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        mProgressDialog.dismiss()
+    }
+
+    fun getCurrentUserID(): String{
+        if(FirebaseAuth.getInstance().currentUser!=null) {
+            return FirebaseAuth.getInstance().currentUser!!.uid
+        }
+        else{
+            Toast.makeText(currContext,"Not signed in yet",Toast.LENGTH_SHORT).show()
+        }
+        return ""
+    }
+
+
+    fun showError(message: String){
+        val req = activity?.findViewById<View>(android.R.id.content)
+        if(req !=null) {
+            val snackBar = Snackbar.make(req, message, Snackbar.LENGTH_LONG)
+            snackBar.view.setBackgroundColor(
+                ContextCompat.getColor(
+                    currContext,
+                    R.color.purple_500
+                )
+            )
+            snackBar.setTextColor(R.color.white)
+            snackBar.show()
+        }
+    }
+}
--- app/src/main/java/com/bottlerunner/room_o_swap/SignInFragment.kt
+++ app/src/main/java/com/bottlerunner/room_o_swap/SignInFragment.kt
@@ -1,31 +1,91 @@
 package com.bottlerunner.room_o_swap
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.bottlerunner.room_o_swap.databinding.FragmentSignInBinding
 import com.bottlerunner.room_o_swap.databinding.FragmentSignUpBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
-class SignInFragment : Fragment() {
+class SignInFragment : BaseFragment(R.layout.fragment_sign_in) {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email:String
+    private lateinit var password: String
+    private lateinit var name :String
+    private lateinit var hostel: String
+    private var roomNo: Int = 0
+    private lateinit var tietName:TextInputEditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        var binding = DataBindingUtil.inflate<FragmentSignInBinding>(inflater
+        var binding = DataBindingUtil.inflate<FragmentSignUpBinding>(inflater
             ,R.layout.fragment_sign_in,container,false)
 
-        binding.btnSignInSignIn.setOnClickListener {
-
+        binding.btnSignUpSignUpFrag.setOnClickListener {
             Navigation.findNavController(view!!).navigate(R.id.action_signInFragment_to_homeFragment)
         }
 
+        binding.btnSignUpSignUpFrag.setOnClickListener{
+            email = binding.tietEmail.toString()
+            password= binding.textInputLayout2.toString()
+            name =binding.tietName.text.toString()
+            if(validateForm(email,password)){
+                signInUser(email, password)
+            }
+        }
+
         return binding.root
+    }
+
+    fun signInUser(email: String, password: String){
+        Toast.makeText(currContext,"Darling darling stand, stand by me, stand, stand by, stand by me, stand by me, stand by me",
+            Toast.LENGTH_LONG).show()
+        showProgressDialog("Thamba thamba")
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {      //This is lamda function
+            hideProgressDialog()                                                                                        //Know how to make it a regular one
+            if (it.isSuccessful) {
+                val firebaseUser = it.result!!.user!!
+                val registeredEmail = firebaseUser.email!!
+                Toast.makeText(
+                    currContext,
+                    "${firebaseUser.email} you have successfully logged in",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Navigation.findNavController(view!!).navigate(R.id.action_signInFragment_to_homeFragment)
+
+            } else {
+                Toast.makeText(currContext, it.exception?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun validateForm(name: String, email: String, password: String): Boolean{
+        if(name.length == 0){
+            showError("Please enter a valid name")
+            return false
+        }
+        if(email.length == 0){
+            showError("Please enter a valid email")
+            return true
+        }
+        if(password.length == 0){
+            showError("Please enter a valid password")
+            return false
+        }
+        return true
+
     }
 
 }
