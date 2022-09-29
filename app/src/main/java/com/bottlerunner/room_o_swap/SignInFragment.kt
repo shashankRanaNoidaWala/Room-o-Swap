a66959b8c9e432277989a28d52e3b5a2751f9379
shashankRanaNoidaWala <101785829+shashankRanaNoidaWala@users.noreply.github.com>
2022-09-29 22:15:23 +0530

    Final commit

--- app/src/main/java/com/bottlerunner/room_o_swap/SignInFragment.kt
+++ app/src/main/java/com/bottlerunner/room_o_swap/SignInFragment.kt
@@ -1,9 +1,6 @@
 package com.bottlerunner.room_o_swap
 
-import android.content.Intent
 import android.os.Bundle
-import android.provider.ContactsContract
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
